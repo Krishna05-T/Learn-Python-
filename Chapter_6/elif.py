c11fbc76f07acc67f5b261e6d60c8e7665360b6e
Krishna Tirole <krishnatirole05@gmail.com>
2026-01-16 20:43:50 +0530

    IF Else

---
+++ Chapter_6/elif.py
@@ -0,0 +1,10 @@
+age = int(input("Enter the age"))
+
+if(age >= 18):
+    print("You are adult")
+
+elif(age >= 13 and age < 18):
+    print("You are teenager")
+
+else:
+    print("You are child")
---
+++ Chapter_6/multifple-if.py
@@ -0,0 +1,11 @@
+num = int(input("enter number : "))
+#Statement 1
+if(num % 2 == 0):
+    print("Number is even")
+
+    
+#Statement 2
+if(num % 3 == 0):
+    print("Number is divisble by 3")
+else:
+    print("Number is not divisble by 3")
