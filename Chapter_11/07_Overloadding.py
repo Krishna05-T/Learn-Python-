37b0c32f29f84cdeb66957758f34b4997a772c4d
Krishna Tirole <krishnatirole05@gmail.com>
2026-02-10 20:30:35 +0530

    07_Overloadding.py

--- Chapter_11/07_Overloadding.py
+++ Chapter_11/07_Overloadding.py
@@ -11,3 +11,27 @@ a = Number(1)
 b = Number(2)
 
 print(a+b)
+
+
+class string:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"My name is {self.name}"
+    
+
+p1 = string("Krishna")
+print(p1)
+
+
+class Len:
+    def __init__(self, member):
+        self.member = member
+    
+    def __len__(self):
+        return len(self.member)
+    
+
+team = Len(["1","2", "3"])
+print(len(team))
